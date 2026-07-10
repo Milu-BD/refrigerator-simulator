211f00616b073d3194dc264c1457d6d17e670671
Milu-BD <alhifazatmilu@gmail.com>
2026-07-10 14:45:44 +0600

    Update app.py

--- app.py
+++ app.py
@@ -933,7 +933,7 @@ with tab3:
                         "Copy Pulldown Matrix",
                         value=p_df.to_csv(sep="\t", index=False),
                         height=180,
-                        key="copy_pulldown_matrix"
+                        key=f"copy_pulldown_{run_idx}"
                     )
                     
                     # Section B: Display CPT Multivariable Flags Data Matrix
@@ -973,7 +973,7 @@ with tab3:
                             "Copy CPT Matrix",
                             value=cpt_df.to_csv(sep="\t", index=False),
                             height=220,
-                            key="copy_cpt_matrix"
+                            key=f"copy_cpt_{run_idx}"
                         )
                     else:
                         st.warning(
